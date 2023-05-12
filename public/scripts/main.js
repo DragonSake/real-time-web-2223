7ff9b4ead2a592b624f03e164216cfd29f5a6757
DragonSake <40611000+DragonSake@users.noreply.github.com>
2023-05-13 00:02:07 +0200

    Add files via upload

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,63 +4,85 @@ const messages = document.querySelector(".messages");
 const username = prompt("Please enter your Summoner name: ", "") || "Anonymous";
 const socket = io();
 
+// When a user sends a message, broadcast it to the clients
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   addMessage(username + ": " + input.value);
 
+  // Send messages to server
   socket.emit("chat_message", {
     message: input.value
   });
 
+  // Clear input field
   input.value = "";
     return false;
     }, false);
 
+  // When a user joins, set username and broadcast it to the clients
+  socket.on("user_join", function(data) {
+    addMessage("Server:" + data + " just joined Ting's rift!");
+});
+
+  // When a user writes messages it broadcast to the clients
   socket.on("chat_message", function(data) {
       addMessage(data.username + ": " + data.message);
   });
 
-  socket.on("user_join", function(data) {
-      addMessage("Server:" + data + " just joined Ting's rift!");
-  });
-
+  // When a user leaves, broadcast it to the clients
   socket.on("user_leave", function(data) {
       addMessage("Server:" + data + " has left Ting's rift.");
   });
 
+  // When a user joins, bring chat history
   socket.on("history", function (history) {
   history.forEach(function (message) {
     addMessage(message.username + ": " + message.message);
   });
 });
 
-  addMessage("Server: You have joined Ting's rift as '" + username  + "'.");
+  // Send username to server
   socket.emit("user_join", username);
 
+  // Adds a message to the user
+  addMessage("Server: You have joined Ting's rift as '" + username  + "'.");
+
+  // Adds messages to the chat with a username
   function addMessage(message) {
-    const li = document.createElement("li"); // Maakt een list item
+    // Creates a list item
+    const li = document.createElement("li");
       
-    // Zet username en message in twee verschillende spans
+    // Set span elements
     const user = document.createElement("span");
     const msg = document.createElement("span");
+
+    // Add classes to the span elements
     msg.classList.add("message");
     user.classList.add("username");
+
+    // Set innerHTML
     user.innerHTML = getUsernameFromMessage(message);
     msg.innerHTML = ": " + message.substring(message.indexOf(": ") + 1);  
-    li.appendChild(user); // Append incoming message to the list item
-    li.appendChild(msg); // Append message to the list item
+
+    // Append span elements to the list item
+    li.appendChild(user);
+    li.appendChild(msg);
   
+    // If the username is the same as the user, add class my-message, else add class other-message
     if (username == user.innerHTML) {
       li.classList.add("my-message");
     }else{
       li.classList.add("other-message");
     }
   
-    // li.innerHTML = message; // Append the rest of the message after the username
+    // Append list item to the messages
     messages.appendChild(li);
+
+    // Scroll to bottom of messages
     window.scrollTo(0, document.body.scrollHeight);
   }
 
+// Returns the username from a message
 function getUsernameFromMessage(message) {
   return message.substring(0, message.indexOf(":"));
 }
