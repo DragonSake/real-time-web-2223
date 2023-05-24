9842f7d73a9d51b07ff0d67e3eb477e4aa82710c
DragonSake <40611000+DragonSake@users.noreply.github.com>
2023-05-24 17:25:14 +0200

    Added base for core functionality (guessing right champion)

---
+++ public/scripts/main.js
@@ -0,0 +1,98 @@
+const form = document.querySelector("form");
+const input = document.querySelector(".input");
+const messages = document.querySelector(".messages");
+const username = prompt("Please enter your Summoner name: ", "") || "Anonymous";
+const socket = io();
+
+// When a user sends a message, broadcast it to the clients
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  addMessage(username + ": " + input.value);
+
+  // Send messages to server
+  socket.emit("chat_message", {
+    message: input.value
+  });
+
+  input.value = "";
+    return false;    
+    }, 
+    
+  false);
+
+  // When a user joins, set username and broadcast it to the clients
+  socket.on("user_join", function(data) {
+    addMessage("Server:" + data + " just joined Ting's rift!");
+});
+
+  // When a user writes messages it broadcast to the clients
+  socket.on("chat_message", function(data) {
+      addMessage(data.username + ": " + data.message);
+  });
+
+  // When a user leaves, broadcast it to the clients
+  socket.on("user_leave", function(data) {
+      addMessage("Server:" + data + " has left Ting's rift.");
+  });
+
+  // When a user joins, bring chat history
+  socket.on("history", function (history) {
+  history.forEach(function (message) {
+    addMessage(message.username + ": " + message.message);
+  });
+});
+
+  // Send username to server
+  socket.emit("user_join", username);
+
+  // Adds a message to the user
+  addMessage("Server: You have joined Ting's rift as '" + username  + "'.");
+
+  // Adds messages to the chat with a username
+  function addMessage(message) {
+    // Creates a list item
+    const li = document.createElement("li");
+      
+    // Set span elements
+    const user = document.createElement("span");
+    const msg = document.createElement("span");
+
+    // Add classes to the span elements
+    msg.classList.add("message");
+    user.classList.add("username");
+
+    // Set innerHTML
+    user.innerHTML = getUsernameFromMessage(message);
+    msg.innerHTML = ": " + message.substring(message.indexOf(": ") + 1);  
+
+    // Append span elements to the list item
+    li.appendChild(user);
+    li.appendChild(msg);
+  
+    // If the username is the same as the user, add class my-message, else add class other-message
+    if (username == user.innerHTML) {
+      li.classList.add("my-message");
+    }else{
+      li.classList.add("other-message");
+    }
+  
+    // Append list item to the messages
+    messages.appendChild(li);
+
+    // Scroll to bottom of messages
+    messages.scrollTop = messages.scrollHeight;
+  }
+
+// Returns the username from a message
+function getUsernameFromMessage(message) {
+  return message.substring(0, message.indexOf(":"));
+}
+
+// socket guessed_right_champion username alert
+socket.on("guessed_right_champion", function (data) {
+  console.log()
+  // alert(username + " guessed right!");
+  // Adds a message to the user
+  addMessage("Server: " + username + " guessed right!");
+});
+
---
+++ server.js
@@ -0,0 +1,109 @@
+// Import modules
+import { Server } from 'socket.io';
+import express from 'express';
+import httpModule from 'http';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const http = httpModule.createServer(app);
+const io = new Server(http);
+const port = process.env.PORT || 3000;
+
+// Public folder
+app.use(express.static('public'))
+
+// View engine
+app.set('view engine', 'ejs');
+
+// Render index.ejs
+app.get("/", function(req, res) {
+    res.render('index', {taggies: tag, championName: randomChampion.name}); // render 'index' with the tag variable
+});
+
+// Declare randomChampion and tag
+let randomChampion;
+let tag = "";
+
+// Fetch data from API
+async function fetchData() {
+    const url = "http://ddragon.leagueoflegends.com/cdn/13.7.1/data/en_US/champion.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    const champions = Object.values(data.data);
+
+    // Returns an array of champions and their tags
+    const filteredChampions = champions.filter((champion) => {
+        return champion
+    }).map(({ name, tags }) => ({ name, tags }));
+
+    // Returns an array of tags
+    const tags = filteredChampions.flatMap((champion) => {
+        return champion.tags;
+    });
+
+    // Returns a random tag from the tags array
+    randomChampion = filteredChampions[Math.floor(Math.random() * filteredChampions.length)];
+    console.log(randomChampion.name)
+    console.log(randomChampion.tags)
+
+    // Set tag to Random tag(s): + randomChampion.tags
+    tag = "Random tag(s): " + randomChampion.tags;
+    return filteredChampions;
+}
+
+// Call fetchData() function
+fetchData();
+
+// Chat history amount of messages and an empty array
+const historySize = 50;
+let history = [];
+
+// When a user connects, fetch data and emit it to the client
+io.on("connection", function (socket) {
+  socket.on("fetchChampionData", () => {
+    fetchData();
+  });
+
+  // When a user joins, set username and broadcast it to the client
+  socket.on("user_join", function (data) {
+    this.username = data;
+    socket.broadcast.emit("user_join", data);
+
+    // Send chat history to the new connected user
+    socket.emit("history", history);
+    // Send random champion name to the new connected user
+    socket.emit("random_champion", randomChampion.name);
+  });
+
+  // When a user sends a message, broadcast it to the clients
+  socket.on("chat_message", function (data) {
+    data.username = this.username;
+    socket.broadcast.emit("chat_message", data);
+
+    // Add new message to history array
+    history.push(data);
+
+    // Remove oldest message if history array has more than 49 messages
+    if (history.length > historySize) {
+      history.shift();
+    }
+
+    // if message is equal to randomChampion.name, alert to every client that the champion has been guessed by username
+    if (data.message == randomChampion.name) {
+      socket.emit("guessed_right_champion");
+    }
+  });
+
+  // When a user disconnects, broadcast it to the clients
+  socket.on("disconnect", function (data) {
+    socket.broadcast.emit("user_leave", this.username);
+  });
+});
+
+// Listen on port 3000
+http.listen(port, function() {
+    console.log("Listening on 127.0.0.1:" + port);
+});
